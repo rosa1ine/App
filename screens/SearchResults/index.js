0fbe59f429a4285f2836c3a60b0f0aa0e15a9cd3
Ayaulym Sayakova <ayaulym.sayakova13@gmail.com>
2023-11-24 22:01:29 +0600

    f

--- screens/SearchResults/index.js
+++ screens/SearchResults/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import MapView from 'react-native-maps';
 import {View, Text} from "react-native";
-import HomeMap from "../../components/HomeMap";
+import HomeMap from "../../components/RouteMap";
 import CarTypes from "../../components/CarTypes";
 
 const SearchResults = (props) => {
     return (
         <View>
             <CarTypes />
-            <HomeMap />
+            <RouteMap />
         </View>
     );
 };
