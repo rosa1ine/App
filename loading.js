78cca7b5352bb98c8327393b1d41433803694502
Kamila <ksalimzh@gmail.com>
2023-11-25 16:09:50 +0600

    loading js file

---
+++ loading.js
@@ -0,0 +1,22 @@
+// Loading.js
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#00FF00" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00FF00', // Green background color
+  },
+});
+
+export default Loading;
