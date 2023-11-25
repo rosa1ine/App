e5c43e0ec23b31e9df432129c2ef35a9a91b4f3b
Kamila <ksalimzh@gmail.com>
2023-11-25 17:18:12 +0600

    no message

--- components/CarTypes/index.js
+++ components/CarTypes/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { View, Text, Pressable } from "react-native";
-import styles from './styles.js';
-import CarTypeRow from "../CarTypeRow/index.js";
-import typesData from '../../assets/data/types';
-import HomeMap from '../HomeMap/existing.js';
-
-const CarTypes = (props) => {
-  const confirm = () => {
-    console.warn('confirm');
-  };
-
-  return (
-    <View style = {{flex: 1}}>
-        <HomeMap />
-       {typesData.map((type) => (
-        <CarTypeRow type = {type}/> ))}
-
-        <Pressable onPress = {confirm} style = {{
-          backgroundColor: 'black',
-          padding: 10,
-          margin: 10,
-          alignItems: 'center'
-        }}> 
-          <Text style= {{color: 'white', fontWeight: 'bold'}}> 
-            Confirm Taxi
-          </Text>
-        </Pressable>
-    </View>
-  );
-};
-
-=======
 import React from "react";
 import { View, Text, Pressable } from "react-native";
 import styles from './styles.js';
@@ -67,5 +33,4 @@ const CarTypes = (props) => {
   );
 };
 
->>>>>>> 6ad53b49086065c249b79f74f14a7ff22a4ffe51
 export default CarTypes;
